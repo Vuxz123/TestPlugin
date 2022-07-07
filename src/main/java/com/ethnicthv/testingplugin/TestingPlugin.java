09f953cb735a1d4cbbbb56b0ff36d9e8d84bd56c
Vuxz123 <72179035+Vuxz123@users.noreply.github.com>
2022-07-07 15:22:17 +0700

    -Minor Fix-

--- src/main/java/com/ethnicthv/testingplugin/TestingPlugin.java
+++ src/main/java/com/ethnicthv/testingplugin/TestingPlugin.java
@@ -2,12 +2,8 @@ package com.ethnicthv.testingplugin;
 
 import com.ethnicthv.testingplugin.manager.Manager;
 import com.ethnicthv.testingplugin.sickness.disease.Benh;
-import org.bukkit.command.Command;
-import org.bukkit.command.CommandExecutor;
-import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
-import org.jetbrains.annotations.NotNull;
 
 
 public final class TestingPlugin extends JavaPlugin {
@@ -29,25 +25,19 @@ public final class TestingPlugin extends JavaPlugin {
     }
 
     public void test(){
-        this.getCommand("test").setExecutor(new CommandExecutor() {
-            @Override
-            public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, @NotNull String[] args) {
-                if(sender instanceof Player){
-                    manager.addDisease(((Player) sender).getUniqueId(), new Benh());
-                    return true;
-                }
-                return false;
+        this.getCommand("test").setExecutor((sender, command, label, args) -> {
+            if(sender instanceof Player){
+                manager.addDisease(((Player) sender).getUniqueId(), new Benh());
+                return true;
             }
+            return false;
         });
-        this.getCommand("datalist").setExecutor(new CommandExecutor() {
-            @Override
-            public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, @NotNull String[] args) {
-                if(sender instanceof Player){
-                    ((Player) sender).sendMessage(manager.toString());
-                    return true;
-                }
-                return false;
+        this.getCommand("datalist").setExecutor((sender, command, label, args) -> {
+            if(sender instanceof Player){
+                sender.sendMessage(manager.toString());
+                return true;
             }
+            return false;
         });
     }
 }
